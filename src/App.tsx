b3b78fab2f93e7c4dd818a8e3b9e79cc789dc39a
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2026-02-06 23:25:40 +0900

    デバッグ用のコードを削除

--- src/App.tsx
+++ src/App.tsx
@@ -22,15 +22,6 @@ function App() {
     fileInputRef.current?.click();
   };
 
-  useEffect(() => {
-    // rakugaki (canvasRef) に何か描画していないか確認
-    console.log('rakugakiRef:', rakugakiRef.current);
-    console.log('faceRef:', faceRef.current);
-  
-    // 同じ ref を参照していないか
-    console.log('同一か:', rakugakiRef.current === faceRef.current);
-  }, []);
-
   // 画像の描画とグレースケール適用
   const drawToCanvas = useCallback((img: HTMLImageElement | HTMLVideoElement) => {
     const canvas = faceRef.current;
